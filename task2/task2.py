b28f19682898195762d312ccfa824ab4a2655526
Adamasmen <88251237+Adamasmen@users.noreply.github.com>
2021-09-12 03:22:26 +0300

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -1,26 +1,18 @@
 from math import sqrt
-import sys
 
 def func(x,y,xc,yc,r):
     return (x - xc) * (x - xc) + (y - yc) * (y - yc) <= r * r
-
-nums = []
-
-with open(sys.argv[1]) as vvod1 :
-    for line in vvod1:
-        nums.append(int(line))
-        xc=(int[0])
-        yc=(int[1])
-        r=(int[3])
-
-numbs = []
-
-with open(sys.argv[2]) as vvod2 :
-    for line in vvod2:
-        numbs.append(int(line))
-        x=(int[0])
-        y=(int[1])
-        func(x,y,xc,yc,r)
+f1 = open("f1.txt", "r")
+content = f1.read()
+f2 = open("f2.txt", "r")
+content1 = f2.read()
+if __name__ == '__main__':
+    x=float(int(content1[0]))
+    y=float(int(content1[1]))
+    xc=float(int(content[0]))
+    yc=float(int(content[1]))
+    r=float(int(content[3]))
+    func(x,y,xc,yc,r)
     if func(x, y, xc, yc, r):
         print("YES")
     else:
