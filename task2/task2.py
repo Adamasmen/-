b1d303863cf991e56c16a62288ec7587158ecd0b
Adamasmen <88251237+Adamasmen@users.noreply.github.com>
2021-09-12 20:15:43 +0300

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -1,19 +1,25 @@
 from math import sqrt
+import sys
 
 def func(x,y,xc,yc,r):
-    return (x - xc) * (x - xc) + (y - yc) * (y - yc) <= r * r
-f1 = open("f1.txt", "r")
-content = f1.read()
-f2 = open("f2.txt", "r")
-content1 = f2.read()
-if __name__ == '__main__':
-    x=float(int(content1[0]))
-    y=float(int(content1[1]))
-    xc=float(int(content[0]))
-    yc=float(int(content[1]))
-    r=float(int(content[3]))
-    func(x,y,xc,yc,r)
-    if func(x, y, xc, yc, r):
-        print("YES")
-    else:
-        print("NO")
+    coord = (x - xc) * (x - xc) + (y - yc) * (y - yc) 
+    if coord == r * r :
+        print(0)
+    elif coord < r * r :
+        print(1)
+    else: 
+        print(2)
+f1 = open(sys.argv[1], "r")
+content = f1.readline()
+xc=float(content.split()[0])
+yc=float(content.split()[1])
+content = f1.readline()
+r=float(content)
+with open(sys.argv[2], "r") as f2:
+    for line in f2 :
+        x=float(line.split()[0])
+        y=float(line.split()[1])
+        func(x, y, xc, yc, r)
+
+
+
